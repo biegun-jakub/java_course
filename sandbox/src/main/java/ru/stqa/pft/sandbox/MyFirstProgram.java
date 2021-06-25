92f77d15b48ef4af282b28bf842392654f508163
yaqb1 <biegun.jakub@gmail.com>
2021-06-25 10:01:53 +0200

    Dodano klasy dla prostokąta i kwadratu

---
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		hello("world");
+		hello("user");
+		hello("Jakub");
+
+		Square s = new Square(5);
+		System.out.println("Powierzchnia kwadratu o boku " + s.l + " = " + s.area());
+
+		Rectangle r = new Rectangle(4, 6);
+		System.out.println("Powierzchnia prostokąta o bokach " + r.a + " i " + r.b + " = " + r.area());
+	}
+
+	public static void hello(String somebody) {
+		System.out.println("Hello, " + somebody + "!");
+	}
+
+}
