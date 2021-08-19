b800420d78fba3f3cc4cbc1cebe2967f1f6fda45
yaqb1 <32592543+yaqb1@users.noreply.github.com>
2021-08-19 12:18:28 +0200

    Dodano obsługę czytania danych testowych z pliku XML

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTests.java
@@ -1,13 +1,16 @@
 package ru.stqa.pft.addressbook.tests;
 
+import com.thoughtworks.xstream.XStream;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.model.GroupData;
 import ru.stqa.pft.addressbook.model.Groups;
 
 import javax.xml.crypto.Data;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.*;
@@ -16,12 +19,18 @@ import static org.hamcrest.MatcherAssert.*;
 public class GroupCreationTests extends TestBase {
 
     @DataProvider
-    public Iterator<Object[]> validGroups() {
-        List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[] {new GroupData().withName("test1").withHeader("header1").withFooter("footer1")});
-        list.add(new Object[] {new GroupData().withName("test2").withHeader("header2").withFooter("footer2")});
-        list.add(new Object[] {new GroupData().withName("test3").withHeader("header3").withFooter("footer3")});
-        return list.iterator();
+    public Iterator<Object[]> validGroups() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.xml")));
+        String xml = "";
+        String line = reader.readLine();
+        while (line != null) {
+            xml += line;
+            line = reader.readLine();
+        }
+        XStream xstream = new XStream();
+        xstream.processAnnotations(GroupData.class);
+        List<GroupData> groups = (List<GroupData>) xstream.fromXML(xml);
+        return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
 
     @Test(dataProvider = "validGroups")
