9baa8e19d1bd5c2f95c3f11890a0cc270f63dec7
yaqb1 <32592543+yaqb1@users.noreply.github.com>
2021-08-20 15:11:19 +0200

    Dodano nowy test porownujacy dane z bazy danych z danymi z UI, dodano wlasciwosc systemowa, ktora okresla czy test ma byc uruchamiany

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -5,10 +5,16 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.*;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
 
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
+import java.util.stream.Collectors;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class TestBase {
@@ -36,5 +42,16 @@ public class TestBase {
     @AfterMethod(alwaysRun = true)
     public void logTestStop(Method m) {
         logger.info("Stop test " + m.getName());
+    }
+
+    public void verifyGroupListInUI() {
+        if (Boolean.getBoolean("verifyUI")) {
+            Groups dbGroups = app.db().groups();
+            Groups uiGroups = app.group().all();
+            assertThat(uiGroups, equalTo(dbGroups.stream().map((g) -> new GroupData()
+                    .withId(g.getId())
+                    .withName(g.getName()))
+                    .collect(Collectors.toSet())));
+        }
     }
 }
