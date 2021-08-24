12936c64f6b87354c0fddd277fe6da4ff538c064
yaqb1 <32592543+yaqb1@users.noreply.github.com>
2021-08-24 16:17:32 +0200

    Dodano test rejestracji nowego użytkownika

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/appmanager/ApplicationManager.java
@@ -18,6 +18,8 @@ public class ApplicationManager {
 
     private String browser;
     private RegistrationHelper registrationHelper;
+    private FtpHelper ftp;
+    private MailHelper mailHelper;
 
     public ApplicationManager(String browser) {
         this.browser = browser;
@@ -50,6 +52,13 @@ public class ApplicationManager {
         return registrationHelper;
     }
 
+    public FtpHelper ftp() {
+        if (ftp == null) {
+            ftp = new FtpHelper(this);
+        }
+        return ftp;
+    }
+
     public WebDriver getDriver() {
         if (driver == null) {
             if (browser.equals(BrowserType.FIREFOX)) {
@@ -63,5 +72,12 @@ public class ApplicationManager {
             driver.get(properties.getProperty("web.baseUrl"));
         }
         return driver;
+    }
+
+    public MailHelper mail() {
+        if (mailHelper == null) {
+            mailHelper = new MailHelper(this);
+        }
+        return mailHelper;
     }
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -3,6 +3,8 @@ package ru.stqa.pft.mantis.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
+
+import java.io.File;
 import java.io.IOException;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
@@ -15,10 +17,13 @@ public class TestBase {
     @BeforeSuite(alwaysRun = true)
     public void setUp() throws IOException {
         app.init();
+        app.ftp().upload(new File
+                ("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak" );
     }
 
     @AfterSuite(alwaysRun = true)
-    public void tearDown() {
+    public void tearDown() throws IOException {
+        app.ftp().restore("config_inc.php.bak", "config_inc.php");
         app.stop();
     }
 }
