7ce8ebcdf93ff947308e8a4904d6fdb015e78283
yaqb1 <32592543+yaqb1@users.noreply.github.com>
2021-08-30 17:42:29 +0200

    Dodano metodę sprawdzającą status zgłoszonego błędu i pomijającą testy, jeżeli zgłoszenie nadal jest otwarte

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
@@ -3,17 +3,21 @@ package ru.stqa.pft.mantis.tests;
 import org.testng.annotations.Test;
 import ru.stqa.pft.mantis.appmanager.HttpSession;
 
+import javax.xml.rpc.ServiceException;
 import java.io.IOException;
+import java.math.BigInteger;
 
 import static org.testng.Assert.assertTrue;
 
 public class LoginTests extends TestBase {
 
     @Test
-    public void testLogin() throws IOException {
+    public void testLogin() throws IOException, ServiceException {
+        skipIfNotFixed(BigInteger.valueOf(0000001));
         HttpSession session = app.newSession();
-        System.out.println(session.login("administrator", "root"));
-        assertTrue(session.login("administrator", "root"));
+        assertTrue(session.login(app.getProperty("web.adminLogin"), app.getProperty("web.adminPassword")));
         assertTrue(session.isLoggedInAs("administrator"));
     }
+
+
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,12 +1,21 @@
 package ru.stqa.pft.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.ObjectRef;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
+
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+import javax.xml.rpc.ServiceException;
 
 
 public class TestBase {
@@ -25,5 +34,21 @@ public class TestBase {
     public void tearDown() throws IOException {
         app.ftp().restore("config_inc.php.bak", "config_inc.php");
         app.stop();
+    }
+
+    public boolean isIssueOpen(BigInteger issueId) throws RemoteException, ServiceException, MalformedURLException {
+        IssueData issue = app.soap().getIssue(issueId);
+        ObjectRef status = issue.getStatus();
+        if (String.valueOf(status) == "resolved") {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public void skipIfNotFixed(BigInteger issueId) throws RemoteException, ServiceException, MalformedURLException {
+        if (isIssueOpen(issueId)) {
+            throw new SkipException("Ignored because of issue " + issueId);
+        }
     }
 }
